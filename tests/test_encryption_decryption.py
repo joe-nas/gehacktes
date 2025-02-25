0212daf09f9786893a49fc06e6e63e61ef5b7ca6
joe-nas <jonas@falcken.de>
2025-02-25 13:36:38 +0100

    tests work as expected

--- tests/test_encryption_decryption.py
+++ tests/test_encryption_decryption.py
@@ -1,49 +1,51 @@
 import myencryption.encryption as enc
+import unittest
 
 encoder_str = enc.encoder_str
 
 
-def testTextEncoding():
-    message = "HALLO123_"
-    assert enc.textEncoding(message, encoder_str) == [
-        17,
-        10,
-        21,
-        21,
-        24,
-        1,
-        2,
-        3,
-        40,
-    ]
+class TestEncryption(unittest.TestCase):
 
+    def testTextEncoding(self):
+        message = "HALLO123_"
+        result = enc.textEncoding(message, encoder_str)
+        expected = [
+            17,
+            10,
+            21,
+            21,
+            24,
+            1,
+            2,
+            3,
+            40,
+        ]
+        self.assertListEqual(result, expected)
 
-def testCreateKeys():
-    encoded_message = [
-        17,
-        10,
-        21,
-        21,
-        24,
-        1,
-        2,
-        3,
-        40,
-    ]
+    def testCreateKeys(self):
+        encoded_message = [
+            17,
+            10,
+            21,
+            21,
+            24,
+            1,
+            2,
+            3,
+            40,
+        ]
 
-    result = enc.createKeys(encoded_message)
+        result = enc.createKeys(encoded_message)
 
-    print(result is tuple)
+        self.assertIsInstance(result, tuple)
+        self.assertIsInstance(result[0], list)
+        self.assertIsInstance(result[1], list)
+        self.assertEqual(len(result[0]), 9)
+        self.assertEqual(len(result[1]), 9)
 
-    assert isinstance(result, tuple)
-    assert isinstance(result[0], list)
-    assert isinstance(result[1], list)
-    assert len(result[0]) == 9
-    assert len(result[1]) == 9
-
-
-def testDecryptmessage():
-    key1 = [13, 4, 15, 10, 8, 0, 2, 3, 14]
-    key2 = [4, 6, 6, 11, 16, 1, 0, 0, 26]
-
-    assert enc.decryptMessage(encoder_str, key1, key2) == "HALLO123_"
+    def testDecryptmessage(self):
+        key1 = [13, 4, 15, 10, 8, 0, 2, 3, 14]
+        key2 = [4, 6, 6, 11, 16, 1, 0, 0, 26]
+        expected = "HALLO123_"
+        result = enc.decryptMessage(encoder_str, key1, key2)
+        self.assertEqual(expected, result)
