dc7ac6313b6a6516bc3b1adca0c5378f8aff09e1
joe-nas <jonas@falcken.de>
2025-02-25 13:59:03 +0100

    repaired tests

--- tests/test_numberSystemConverter.py
+++ tests/test_numberSystemConverter.py
@@ -1,5 +1,5 @@
 import unittest
-import numberConverter as nsc
+import numberConverter.RecursiveNumberSystemConverter as nsc
 
 
 hexLookUpDict = nsc.hexLookUpDict
@@ -11,14 +11,12 @@ class TestRecursiveFromBaseToDecimalConversion(unittest.TestCase):
         return super().setUp()
 
     def test_recursiveFromBaseToDecimalConversion(self):
-
         self.assertEqual(nsc.recursiveFromBaseToDecimalConversion("110011", 2, 0), 51)
         self.assertEqual(nsc.recursiveFromBaseToDecimalConversion("ZA7", 36, 0), 45727)
 
     def test_recursiveFromDecimalToBase(self):
-
         self.assertEqual(nsc.recursiveFromDecimalToBase(212, 2, []), 11010100)
         self.assertEqual(nsc.recursiveFromDecimalToBase(255, 2, []), 11111111)
 
     def test_combinedRecursiveConversion(self):
-        self.assertEqual(nsc.combinedRecursiveConversion())
+        self.assertEqual(nsc.combinedRecursiveConversion("AFFE", 16, 10), 45054)
